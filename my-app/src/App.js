b048c72286a194c1f2916ceceed2337951f19587
Orkhan-Abdullazada <tu7pnc7lt@code.edu.az>
2023-12-14 14:15:02 +0400

    Sa

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './component/adminsite/Header';
+
+function App() {
+  return (
+    <div className="App">
+          <Headers></Headers>
+    </div>
+  );
+}
+
+export default App;
